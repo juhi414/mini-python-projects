41f86f8985504333715713f78c5b2e2f641d24d3
juhi414 <juhii9122@gmail.com>
2025-06-14 21:25:27 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("Welcome to my software-quiz-python!")
+
+playing = input("DO YOU WANT TO PLAY? ")
+if playing != "yes":
+    quit()
+print("OKAY! LET'S START.")
+
+answer = input("What does SDLC stand for? ")
+if answer == "software development life cycle" :
+    print("CORRECT") 
+else:
+    print("WRONG") 
+  
+
+
+    
